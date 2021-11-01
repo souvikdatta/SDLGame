bb2777dc7bfbf0fbadce0ebbf3046af5c6b4c166
Souvik <sd.souvikdatta@gmail.com>
2021-11-01 16:03:17 +0530

    Added code to use polymorphism (Not pure virtual functions) to loop through objects of GameObject base class types

--- game.cpp
+++ game.cpp
@@ -37,61 +37,24 @@ bool Game::init(const string nameOfWindow, const int xPos, \
                 //Set render color
                 SDL_SetRenderDrawColor (m_pRenderer, 255, 0, 0, 255);
 
+
                 if (TextureManager::getInstance().load ("assets/animate-alpha.png", \
                     "animate", m_pRenderer) == false)
                 {
                     retStatus =  false;
                 }
 
+                //Create GameObject and PLayer Object
+                m_GameObjects.push_back (new GameObject());
+                //Call Load
+                m_GameObjects[0]->load ("animate", 100, 100, 128, 82);
 
-#if 0
-                //Load the assets
-                //SDL_Surface *pTempSurface = SDL_LoadBMP ("assets/animate.bmp");
-                SDL_Surface *pTempSurface = IMG_Load ("assets/animate-alpha.png");
-                if(pTempSurface)
-                {
-                    //From the Surface, we create a Texture
-                    m_pTexture = SDL_CreateTextureFromSurface (m_pRenderer, pTempSurface);
-                    if(m_pTexture)
-                    {
-                        //Free the Surface as I donot need the Surface any more - why?
-                        //Because I have already created a Texture from the Surface
-                        SDL_FreeSurface (pTempSurface);
+                m_GameObjects.push_back (new Player());
+                //Call Load
+                m_GameObjects[1]->load ("animate", 350, 350, 128, 82);
 
-                        SDL_QueryTexture (m_pTexture, NULL, NULL, \
-                            &m_sourceRectangle.w, &m_sourceRectangle.h);
-
-                        //Now setting the position and size of dest rect according to source rect
-                        // m_destinationRectangle.x = m_sourceRectangle.x = 0;
-                        // m_destinationRectangle.y = m_sourceRectangle.y = 0;
-                        // m_destinationRectangle.w = m_sourceRectangle.w = 50;
-                        // m_destinationRectangle.h = m_sourceRectangle.h = 50;
-
-                        m_sourceRectangle.x = 0;
-                        m_sourceRectangle.y = 0;
-                        m_destinationRectangle.x=0;
-                        m_destinationRectangle.y=0;
-
-                        m_sourceRectangle.w = 128;
-                        m_sourceRectangle.h = 82;
-
-                        m_destinationRectangle.w = 128;
-                        m_destinationRectangle.h = 82;
-                    }
-                    else
-                    {
-                        retStatus =  false;
-                        cout << "Unable to create Texture from Surface" << endl;
-                    }   
-                }
-                else
-                {
-                    retStatus =  false;
-                    cout << "Unable to create Surface" << endl;
-                }
-#endif
+                //Now loop through the objects and call load functions of respective objects
             }
-
         }
         else 
         {
@@ -112,36 +75,40 @@ void Game::render()
 {
     SDL_RenderClear (m_pRenderer);
 
-    //Draw the Texture to the window
-    //SDL_RenderCopy (m_pRenderer, m_pTexture, \
-        &m_sourceRectangle, &m_destinationRectangle);
+    // TextureManager::getInstance().draw ("animate", 0, 0, 128, 82, m_pRenderer);
 
-    //SDL_RenderCopyEx (m_pRenderer, m_pTexture, \
-        &m_sourceRectangle, &m_destinationRectangle, 0, 0, SDL_FLIP_HORIZONTAL);
+    // TextureManager::getInstance().drawFrame ("animate", 100, 100, \
+    //     128, 82, m_pRenderer, 1, m_currFrameIndex);
 
-    TextureManager::getInstance().draw ("animate", 0, 0, 128, 82, m_pRenderer);
+    // m_go.draw (m_pRenderer);
+    // m_player.draw (m_pRenderer);
 
-    TextureManager::getInstance().drawFrame ("animate", 100, 100, \
-        128, 82, m_pRenderer, 1, m_currFrameIndex);
+    for(auto &i: m_GameObjects)
+        i->draw(m_pRenderer);
 
     SDL_RenderPresent (m_pRenderer);
 }
 
 void Game::update()
 {
+#if 0
     static unsigned int currTime, lastTime = 0;
     static int index = 0;
     currTime = SDL_GetTicks();
     if(currTime >= lastTime + animDelayInMS)
     {
         //Move the source window in x Axis
-        //m_sourceRectangle.x = 128 * (index++ % 6);
         m_currFrameIndex = index++ % 6;
-        //cout << m_sourceRectangle.x << "\n";
         lastTime = currTime;
         if(index > 6)
             index = 0;
     }
+#endif
+    // m_go.update();
+    // m_player.update();
+    for(auto &i: m_GameObjects)
+        i->update();
+
 }
 
 void Game::handleEvents()
